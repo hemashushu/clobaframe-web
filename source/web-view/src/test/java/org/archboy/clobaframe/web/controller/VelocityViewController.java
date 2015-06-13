b1551b51f91d8d80f38a5ae0eb57992baab48a67
Hippo Spark <hippospark@gmail.com>
2015-06-13 11:03:39 +0800

    Remove the local root web resource and add web view unit test.

---
+++ source/web-view/src/test/java/org/archboy/clobaframe/web/controller/VelocityViewController.java
@@ -0,0 +1,55 @@
+package org.archboy.clobaframe.web.controller;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import javax.inject.Inject;
+import org.archboy.clobaframe.query.DefaultViewModel;
+import org.archboy.clobaframe.query.ViewModel;
+import org.archboy.clobaframe.web.view.tool.PageHeaderTool;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author yang
+ */
+@Controller
+public class VelocityViewController {
+	
+	@Inject
+	private PageHeaderTool pageHeaderTool;
+	
+	@RequestMapping("/index")
+	public String index(Model model){
+		
+		// add custom page header
+		ViewModel attributes = new DefaultViewModel()
+			.add("href", "/atom.xml")
+			.add("type", "application/atom+xml")
+			.add("rel", "alternate")
+			.add("title", "Clobaframe-web ATOM Feed");
+		pageHeaderTool.addHeader("link", attributes, false);
+		
+		// add model - current time
+		Calendar calendar = Calendar.getInstance();
+		calendar.clear();
+		calendar.set(2015, 6, 10, 18, 5, 30);
+		
+		model.addAttribute("now", calendar.getTime());
+		model.addAttribute("html", "text contains <strong>html tags</strong>, 'single quotes' & \"double quotes\".");
+		model.addAttribute("htmlP", "line one.\nline two.");
+		model.addAttribute("integer", 1234567);
+		
+		// add model - an object
+		Map<String, String> viewObject = new HashMap<String, String>();
+		viewObject.put("id", "001");
+		viewObject.put("name", "foo");
+		
+		model.addAttribute("viewObject", viewObject);
+		
+		return "index";
+	}
+}
---
+++ source/web-view/src/test/resources/webapp/resources/default/jsx/helloworld.js
@@ -0,0 +1,8 @@
+/* 
+ * helloworld.js 
+ */
+
+React.render(
+  <h1>Hello, world!</h1>,
+  document.getElementById('example')
+);
